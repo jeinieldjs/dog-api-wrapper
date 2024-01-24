ec527073d7861fb8537749744e8e80323fff38d4
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-24 16:30:50 +0800

    Added tests for API client controller.

--- test/controllers/api/client_controller_test.rb
+++ test/controllers/api/client_controller_test.rb
@@ -4,4 +4,34 @@ class Api::ClientControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'should get an array of all breeds' do 
+    get api_all_breeds_path
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get a random image' do 
+    get api_random_image_path
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get an array of master breeds' do 
+    get api_all_master_breeds_path
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get an array sub-breed of specified breed' do 
+    get api_sub_breeds_path(breed: 'hound')
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get information on a specified breed' do 
+    get api_breed_info_path(breed: 'hound')
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
 end
