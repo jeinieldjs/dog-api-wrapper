1ef08e238eb1a2b59b3694c9c90ec77aba48b204
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-24 13:03:05 +0800

    Modified client.rb

--- app/api/dog_api/client.rb
+++ app/api/dog_api/client.rb
@@ -6,16 +6,22 @@ module DogApi
             response = Request.call(http_method: 'get', endpoint: 'breeds/list/all' )
         end
         
-        def self.random_breed
-            response = Request.call(http_method: 'get', endpoint: 'list/all/random' )
+        def self.random_image
+            response = Request.call(http_method: 'get', endpoint: 'breeds/image/random' )
         end
 
         def self.all_master_breeds
             response = Request.call(http_method: 'get', endpoint: 'breeds/list' )
         end
 
-        def self.sub_breeds
-            response = Request.call(http_method: 'get', endpoint: 'breed/{breed}/list' )
+        def self.sub_breeds(breed)
+            endpoint = "breed/#{breed}/list"
+            response = Request.call(http_method: 'get', endpoint: endpoint)
+        end
+
+        def self.breed_info(breed)
+            endpoint = "breed/#{breed}"
+            response = Request.call(http_method: 'get', endpoint: endpoint)
         end
     end
 end
